66e98431ce73f138c9a734892438d69704087e12
Michał Mazurek <michal.mazurek@ifresearch.org>
2015-06-03 16:34:20 +0200

    more fixes for date comparison

--- toddler/scheduler.py
+++ toddler/scheduler.py
@@ -46,7 +46,7 @@ class Scheduler(object):
                 "url": crawl_root,
                 "method": "GET",
                 "actions": actions,
-                "timeout": datetime.utcnow().isoformat()
+                "timeout": datetime.now(timezone.utc).isoformat()
             }
 
             host.last_crawl_job_date = datetime.utcnow()
@@ -58,6 +58,10 @@ class Scheduler(object):
     def schedule_jobs_for_hosts(self):
 
         for host in Host.objects:
+            if host.last_crawl_job_date is not None:
+                last_crawl_job_date = host.last_crawl_job_date
+                if last_crawl_job_date.tzinfo is None:
+                    last_crawl_job_date.replace(tzinfo=timezone.utc)
             if (host.last_crawl_job_date is None
                     or host.last_crawl_job_date >=
                         (datetime.now(timezone.utc)+timedelta(1))):
